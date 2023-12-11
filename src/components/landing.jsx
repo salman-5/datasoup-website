0fe5f7798a80257c8cc72a0828f5d5b87c53b567
Salman <fcsalmu234@gmail.com>
2023-12-11 22:28:13 +0100

    completed version 1

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -8,14 +8,14 @@ const Landing = () => {
 
           <div className="text-center text-slate-400 text-[49.61px] font-bold font-inter leading-[65px]">All Your Team<br />Needs in One Great<br />Platform</div>
 
-          <div className="text-center text-teal-900 text-xl font-bold font-inter leading-[30px]">Follow your team’s plans, track work progress,<br />and discuss work all in one place. Let us<br />manage your work flawlessly and be on top of<br />everything your team is up to. </div>
+          <div className="text-center text-teal-900 text-xl font-bold font-inter leading-[30px]">Follow your team’s plans, track work progress,and discuss work all in one place. Let us<br />manage your work flawlessly and be on top of<br />everything your team is up to. </div>
         </div>
 
         <div className='flex justify-center'>
 
 
-          <div className="p-4 bg-neutral-100 rounded-[30px] border border-teal-900 f text-teal-900 text-[17px] font-normal font-inter leading-normal">Watch Demo</div>
-          <div className="p-4 bg-teal-900 rounded-[30px] border border-teal-900 flex text-white text-base font-normal font-inter leading-normal">Try for Free</div>
+          <button className="p-4 bg-neutral-100 rounded-[30px] border border-teal-900 f text-teal-900 text-[17px] font-normal font-inter leading-normal">Watch Demo</button>
+          <button className="p-4 bg-teal-900 rounded-[30px] border border-teal-900 flex text-white text-base font-normal font-inter leading-normal">Try for Free</button>
         </div>
 
       </div>
